818f3041e130aa866ebed53c43e26b9a7945bd6c
tqcss <134121905+tqcss@users.noreply.github.com>
2024-02-21 21:45:55 +0800

    modify: cleaned code

--- js/app.js
+++ js/app.js
@@ -1,4 +1,3 @@
-// Function to create Intersection Observer for animations
 const createIntersectionObserver = (className, animationClass) => {
     return new IntersectionObserver(entries => {
         entries.forEach(entry => {
@@ -11,12 +10,10 @@ const createIntersectionObserver = (className, animationClass) => {
     }, { threshold: 1 });
 };
 
-// Observer for elements with the "anim-right" class
 const observerRight = createIntersectionObserver(".anim-right", "play-right");
 const hiddenElementsRight = document.querySelectorAll(".anim-right");
 hiddenElementsRight.forEach(element => observerRight.observe(element));
 
-// Observer for elements with the "anim-left" class
 const observerLeft = createIntersectionObserver(".anim-left", "play-left");
 const hiddenElementsLeft = document.querySelectorAll(".anim-left");
 hiddenElementsLeft.forEach(element => observerLeft.observe(element));
@@ -24,15 +21,12 @@ hiddenElementsLeft.forEach(element => observerLeft.observe(element));
 const hero = document.querySelector(".hero");
 const moveSpeed = 0.19;
 
-
-// Hero Image interactivity
 let xValue = 0;
 let yValue = 0;
 
-window.addEventListener("mousemove", (event) => {
+window.addEventListener("mousemove", event => {
     xValue = event.clientX - window.innerWidth / 2;
     yValue = event.clientY - window.innerHeight / 2;
-    // console.log(xValue, yValue);
 
     hero.style.transform = `translateX(calc(-50% - ${xValue * moveSpeed}px)) translateY(calc(-50% - ${yValue * moveSpeed}px))`;
-})
+});
